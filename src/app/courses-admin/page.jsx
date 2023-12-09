6d1ee5ee374af4fb2f1eccf96772ad45bd98e819
steveharianto <steveharianto@gmail.com>
2023-12-09 22:01:26 +0700

    add courses

--- src/app/courses-admin/page.jsx
+++ src/app/courses-admin/page.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 
 export default function Page() {
@@ -27,27 +28,27 @@ export default function Page() {
                     </svg>
                 </div>
                 <div className={`${sideBarIconStyle} mt-2`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                     </svg>
                 </div>
                 <div className={`${sideBarIconStyle} mt-2`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                     </svg>
                 </div>
                 <div className={`${sideBarIconStyle} mt-2`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                     </svg>
                 </div>
                 <div className={`${sideBarIconStyle} mt-2`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                     </svg>
                 </div>
                 <div className={`${sideBarIconStyle} mt-2`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path
                             stroke-linecap="round"
                             stroke-linejoin="round"
@@ -56,12 +57,12 @@ export default function Page() {
                     </svg>
                 </div>
                 <div className={`${sideBarIconStyle} mt-2`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                     </svg>
                 </div>
                 <div className={`${sideBarIconStyle} mt-2`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path
                             stroke-linecap="round"
                             stroke-linejoin="round"
@@ -70,7 +71,7 @@ export default function Page() {
                     </svg>
                 </div>
                 <div className={`${sideBarIconStyle} mt-2`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path
                             stroke-linecap="round"
                             stroke-linejoin="round"
@@ -82,11 +83,79 @@ export default function Page() {
             <div className=" min-h-screen w-full flex flex-col">
                 {/* Top Header */}
                 <div className="w-[95%] h-16 shadow-md mx-auto mt-8 rounded-xl border-2 flex flex-row-reverse items-center px-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#a9a6a6" className="w-8 h-8  hover:cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#a9a6a6" class="w-8 h-8  hover:cursor-pointer">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
                 </div>
-                <div className="flex justify-between p-12"></div>
+                <div className="flex justify-between p-10">
+                    <div className="flex flex-col">
+                        <p className="text-2xl mb-4 text-[#5e5873] font-medium">Courses</p>
+                        <div className="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#46a583" className="w-5 h-5 ">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#5e5873" class="w-4 h-4 mx-2">
+                                <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z" clip-rule="evenodd" />
+                            </svg>
+
+                            <p className=" text-[#5e5873] text-sm"> Courses</p>
+                        </div>
+                    </div>
+                </div>
+                <p className="mx-auto mt-4 font-medium text-3xl">Available Courses</p>
+                <div className="min-w-96 mx-auto flex">
+                    <input type="text" className="mx-auto mt-2 text-xs border-[2px] border-gray-300 rounded-lg py-4 px-2 w-96" placeholder="Search For Courses" />
+
+                    <button className="bg-[#46a583] text-white px-8 rounded ms-4 w-auto h-fit py-4 mt-2">+ Courses</button>
+                </div>
+                <div className="w-[85%] mx-auto flex flex-wrap">
+                    {/* Class Card */}
+                    <div className="w-64 mt-4 pb-6 bg-white rounded-lg shadow-lg me-8">
+                        <div className="relative h-44 bg-green-500 rounded-t-lg">
+                            <img className="absolute inset-0 w-full h-full object-cover rounded-t-lg" src="placeholder.jpeg" alt="Card Image" />
+                        </div>
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold text-gray-800">Title</h2>
+                            <p className="mt-2 text-gray-600">Description</p>
+                        </div>
+                    </div>
+                    <div className="w-64 mt-4 pb-6 bg-white rounded-lg shadow-lg me-8">
+                        <div className="relative h-44 bg-green-500 rounded-t-lg">
+                            <img className="absolute inset-0 w-full h-full object-cover rounded-t-lg" src="placeholder.jpeg" alt="Card Image" />
+                        </div>
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold text-gray-800">Title</h2>
+                            <p className="mt-2 text-gray-600">Description</p>
+                        </div>
+                    </div>
+                    <div className="w-64 mt-4 pb-6 bg-white rounded-lg shadow-lg me-8">
+                        <div className="relative h-44 bg-green-500 rounded-t-lg">
+                            <img className="absolute inset-0 w-full h-full object-cover rounded-t-lg" src="placeholder.jpeg" alt="Card Image" />
+                        </div>
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold text-gray-800">Title</h2>
+                            <p className="mt-2 text-gray-600">Description</p>
+                        </div>
+                    </div>
+                    <div className="w-64 mt-4 pb-6 bg-white rounded-lg shadow-lg me-8">
+                        <div className="relative h-44 bg-green-500 rounded-t-lg">
+                            <img className="absolute inset-0 w-full h-full object-cover rounded-t-lg" src="placeholder.jpeg" alt="Card Image" />
+                        </div>
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold text-gray-800">Title</h2>
+                            <p className="mt-2 text-gray-600">Description</p>
+                        </div>
+                    </div>
+                    <div className="w-64 mt-4 pb-6 bg-white rounded-lg shadow-lg me-8">
+                        <div className="relative h-44 bg-green-500 rounded-t-lg">
+                            <img className="absolute inset-0 w-full h-full object-cover rounded-t-lg" src="placeholder.jpeg" alt="Card Image" />
+                        </div>
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold text-gray-800">Title</h2>
+                            <p className="mt-2 text-gray-600">Description</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
